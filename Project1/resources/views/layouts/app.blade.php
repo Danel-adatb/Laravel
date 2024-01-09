527b87fa160fa94f2f9a26aa583fd53b13ee5d3a
Danel-adatb <szadaegyetem@gmail.com>
2024-01-09 19:55:21 +0100

    Section #4 | Project#1 ~ 34. episode

--- Project1/resources/views/layouts/app.blade.php
+++ Project1/resources/views/layouts/app.blade.php
@@ -2,11 +2,19 @@
 <HTML>
     <HEAD>
         <TITLE>Laravel Project No. 1</TITLE>
+        @yield('styles')
     </HEAD>
 
     <BODY>
         <H1>@yield('title')</H1>
         <div>
+             <!-- Flash message -->
+             @if (session()->has('success'))
+                 <div>
+                    {{ session('success') }}
+                 </div>
+             @endif
+
             @yield('content')
         </div>
     </BODY>
--- Project1/routes/web.php
+++ Project1/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,11 +21,20 @@ Route::get('/', function() {
 });
 
 //If you use Query Builder such as the example below, you have to 'execute' the query with the get() method!
+/*->where('completed', true)*/
 Route::get('/tasks', function() {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()/*->where('completed', true)*/->get()
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{id}/edit', function($id) {
+    return view('edit', [
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.edit');
 
 Route::get('/tasks/{id}', function($id) {
     //Laravel Query
@@ -34,5 +45,43 @@ Route::get('/tasks/{id}', function($id) {
         abort(Response::HTTP_NOT_FOUND);
     }*/
 
-    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');
+})->name('tasks.store');
+
+Route::put('/tasks/{id}', function(Request $request, $id) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
